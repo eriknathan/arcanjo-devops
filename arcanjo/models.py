f1546fab0cca2604d68695a916e8c08c9e17a5f3
eriknathan <eriknathan.contato@gmail.com>
2024-02-27 19:18:26 -0300

    feat: criando o owner de cada contato

--- arcanjo/models.py
+++ arcanjo/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     # Retorna o nome da categoria dentro de category no admin do django
     def __str__(self) -> str:
         return f'{self.name}'
-
 
 
 # Isso seria uma "tabela" chamada Contact
@@ -23,6 +27,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     # Retorna o nome do contato dentro de contacts no admin do django
     def __str__(self) -> str:

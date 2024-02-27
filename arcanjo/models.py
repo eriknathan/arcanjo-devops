88f422a402378771373915c2932ff705ec738147
eriknathan <eriknathan.contato@gmail.com>
2024-02-26 21:02:27 -0300

    feat: criando o primeiro models de contact

--- arcanjo/models.py
+++ arcanjo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# Isso seria uma "tabela" chamada Contact
+class Contact(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    phone = models.CharField(max_length=80)
+    email = models.EmailField(max_length=50)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
+    

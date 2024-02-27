6b5d3cb58ec3c9fc142a8d8359ffbacf0f97c3a3
eriknathan <eriknathan.contato@gmail.com>
2024-02-27 20:38:28 -0300

    feat: craindo index.html exibindo contatos

--- arcanjo/views/contact_view.py
+++ arcanjo/views/contact_view.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from arcanjo.models import Contact
 
 def index(request):
+    contacts = Contact.objects.all()
+    context = {'contacts': contacts}
+
     return render(
-        request, 'arcanjo/index.html',
+        request, 'arcanjo/index.html', context
     )

b9115ac23e9b330f4758ecfbce338e58e26d1253
eriknathan <eriknathan.contato@gmail.com>
2024-02-28 20:58:11 -0300

    feat: esquema de pesquisa de contatos

--- arcanjo/views/contact_view.py
+++ arcanjo/views/contact_view.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from arcanjo.models import Contact
 
 
@@ -9,6 +10,28 @@ def index(request):
     context = {'contacts': contacts, 'site_title': 'Contatos - '}
 
     # print(contacts.query)
+    return render(request, 'arcanjo/index.html', context)
+
+
+def search(request):
+    '''realiza a busca do contato'''
+    search_value = request.GET.get('q', '').strip()
+    # print('search_value: ', search_value)
+    if search_value == '':
+        return redirect('arcanjo:index')
+
+    contacts = Contact.objects. \
+        filter(show=True). \
+        filter(Q(first_name__icontains=search_value) |
+               Q(last_name__icontains=search_value) |
+               Q(phone__icontains=search_value) |
+               Q(email__icontains=search_value)) \
+        .order_by('-id')
+
+    print(contacts.query)
+
+    context = {'contacts': contacts, 'site_title': 'Contatos - '}
+    # print(contacts.query)
 
     return render(request, 'arcanjo/index.html', context)
 
@@ -16,11 +39,10 @@ def index(request):
 def contact(request, contact_id):
     '''retorna os dados de um contato via id (pk),
     caso o id não seja encontrado, ele retorna 404'''
-    # single_contact = Contact.objects.filter(pk=contact_id)
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    
     site_title = f'{single_contact.first_name} {single_contact.last_name} - '
     context = {'contact': single_contact, 'site_title': site_title}
 
     # print(single_contact.query)
-
     return render(request, 'arcanjo/contact.html', context)

322c292fb8b5032d9948dd618a9da0a1759cb7fe
eriknathan <eriknathan.contato@gmail.com>
2024-03-01 07:56:37 -0300

    feat: delete e validação de delete

--- arcanjo/views/contact_forms.py
+++ arcanjo/views/contact_forms.py
@@ -61,3 +61,23 @@ def update(request, contact_id):
     }
     # print(contacts.query)
     return render(request, 'contact/create.html', context)
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('arcanjo:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
+    )

d8deba012897b10c5006475d8df1af60fb496123
eriknathan <eriknathan.contato@gmail.com>
2024-03-01 07:02:31 -0300

    fix: tratamento de erros

--- arcanjo/views/contact_forms.py
+++ arcanjo/views/contact_forms.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q
-from arcanjo.models import Contact
-from django.core.paginator import Paginator
+from django.shortcuts import render
+from arcanjo.forms import ContactForm
 
 
 def create(request):
-    context = {}
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+        # print(contacts.query)
+        return render(request, 'contact/create.html', context)
+
+    context = {
+        'form': ContactForm()
+    }
     # print(contacts.query)
     return render(request, 'contact/create.html', context)

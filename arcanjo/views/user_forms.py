16147c3bf3363d084734c9945a90e30e6e714706
eriknathan <eriknathan.contato@gmail.com>
2024-03-03 10:36:41 -0300

    feat: formulário para login do usuário

--- arcanjo/views/user_forms.py
+++ arcanjo/views/user_forms.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from arcanjo.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def register(request):
@@ -14,9 +16,30 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Usuário Registrado!')
-            return redirect('arcanjo:index')
-
+            return redirect('arcanjo:login')
 
     context = {'form': form}
 
     return render(request, 'contact/register.html', context)
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logado com sucesso!')
+            return redirect('arcanjo:index')
+        messages.error(request, 'Login inválido!')
+
+    context = {'form': form}
+    return render(request, 'contact/login.html', context)
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('arcanjo:login')

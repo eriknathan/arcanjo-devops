3a1730ebecfe2baf528516c7d16466e2f697e81c
eriknathan <eriknathan.contato@gmail.com>
2024-03-07 13:13:47 -0300

    feat: atualizando template da adição de projeto

--- services/views/projetos_view.py
+++ services/views/projetos_view.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from services.forms import ProjetoForm
 from services.models import Project
 
@@ -9,11 +10,39 @@ def projects(request):
     return render(request, 'projects/projetos.html', context)
 
 
+# def create_project(request):
+#     if request.method == 'POST':
+#         form = ProjetoForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#     else:
+#         form = ProjetoForm()
+#     return render(request, 'projects/create.html', {'form': form})
+
+
 def create_project(request):
+    form_action = reverse('arcanjo:create')
+
     if request.method == 'POST':
-        form = ProjetoForm(request.POST)
+        form = ProjetoForm(request.POST, request.FILES)
+
+        context = {
+            'form': form,
+            'form_action': form_action,
+        }
+
         if form.is_valid():
-            form.save()
-    else:
-        form = ProjetoForm()
-    return render(request, 'projects/create.html', {'form': form})
+            print("Formulário é válido!")
+            contact = form.save()
+            return redirect('arcanjo:update', contact_id=contact.pk)
+
+        # print(contacts.query)
+        return render(request, 'contact/create.html', context)
+
+    context = {
+        'form': ProjetoForm(),
+        'form_action': form_action,
+
+    }
+    # print(contacts.query)
+    return render(request, 'projects/create.html', context)
